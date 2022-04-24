6fe6f9358456b239633915c789f1e7a4266dbf6f
David Jacquin <davidaj0202@gmail.com>
2022-04-24 09:57:46 -0700

    new

--- Test.py
+++ Test.py
@@ -1,2 +1,8 @@
-print("Hello World")
-print("Yo Yo Yo")
+from turtle import*
+color('red', 'yellow')
+begin_fill()
+while (abs(pos()) < 1) is true:
+    forward(200)
+    left(170)
+end_fill()
+done()
