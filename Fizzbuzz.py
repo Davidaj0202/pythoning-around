6d3d624cc1ccb94bbe3c289f5a33b7993632a699
Davidaj0202 <davidaj0202@gmail.com>
2022-05-28 22:00:28 -0700

    new

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,11 +1,23 @@
-def fizzbuzz(i):
-    if i%3 == 0 and i%5 == 0:
-        return "Fizzbuzz"
-    elif i%3 == 0:
-        return "Fizz"
-    elif i%5 == 0:
-        return "Buzz"
-    else:
-        return i
-for i in range(100):
-    print(fizzbuzz(i))
+# Importing pygame module
+import pygame
+from pygame.locals import *
+ 
+# initiate pygame and give permission
+# to use pygame's functionality.
+pygame.init()
+ 
+# create the display surface object
+# of specific dimension.
+window = pygame.display.set_mode((600, 600))
+ 
+# Fill the scree with white color
+window.fill((255, 255, 255))
+ 
+# Using draw.rect module of
+# pygame to draw the solid rectangle
+pygame.draw.rect(window, (0,   0, 255),
+                 [100, 100, 400, 100], 0)
+ 
+# Draws the surface object to the screen.
+while any:
+    pygame.display.update()
